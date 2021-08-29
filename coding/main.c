23669de241e143199e230c7164ec475bc88cd351
grubgrubik <grubastuk@gmail.com>
2021-08-29 16:38:30 +0300

    have done task1 chapter 4

--- coding/main.c
+++ coding/main.c
@@ -3,6 +3,35 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int count = 0;
+    char ch;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == '\n')
+            printf("\n \\n   - %-4d ", ch);
+
+        else if (ch == '\t')
+            printf(" \\t   - %-4d", ch);
+
+        else if (ch == ' ')
+            printf(" 'sp' - %-4d", ch);
+
+        else
+            printf(" %c   - %4d ", ch, ch);
+
+        if (count == 5)
+        {
+            putchar('\n');
+            count = 0;
+
+        }
+
+
+        count++;
+    }
+
+
+
     return 0;
 }
