5b17bccf11a2e8069a1fd61ad6aad8174c439f48
grubgrubik <grubastuk@gmail.com>
2021-08-30 23:25:13 +0300

    do task1

---
+++ Book_chapters/Chapter4/Task1/task1/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int count = 0;
+    char ch;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == '\n')
+            printf("\n \\n   - %-4d ", ch);
+
+        else if (ch == '\t')
+            printf(" \\t   - %-4d", ch);
+
+        else if (ch == ' ')
+            printf(" 'sp' - %-4d", ch);
+
+        else
+            printf(" %c   - %4d ", ch, ch);
+
+        if (count == 5)
+        {
+            putchar('\n');
+            count = 0;
+        }
+
+        count++;
+    }
+
+    return 0;
+}
