d06621c9ac1253640a106ba9038ae3bf9f1f7960
grubgrubik <grubastuk@gmail.com>
2021-09-03 18:42:47 +0300

    task6 is ready

--- Book_chapters/Chapter4/Task6/codding/main.c
+++ Book_chapters/Chapter4/Task6/codding/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+char get_first(void);
 
 int main()
 {
+    int ch;
+
+    while ((ch = get_first()) != 'l')
+    {
+        printf("line1\n");
+        putchar(ch);
+        printf("line2\n");
+    }
+
     printf("Hello world!\n");
     return 0;
+}
+
+
+
+char get_first(void)
+{
+
+    int ch;
+
+    while ((ch = getchar()) == '\n')
+        continue;
+
+    while (getchar() != '\n')
+        continue;
+    return ch;
 }
