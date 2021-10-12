801a2da6224f5115eb5ff6cd935338f602623740
grubgrubik <grubastuk@gmail.com>
2021-10-13 01:24:41 +0300

    temporary work is done

---
+++ Book_chapters/Chapter9/Work_task/codding/func_describle.h
@@ -0,0 +1,11 @@
+#define MIN_MENU    1
+#define MAX_MENU    4
+
+
+//main menu
+void main_menu(void);
+
+//check correct chose
+int check_menu(void);
+
+
--- Book_chapters/Chapter9/Work_task/codding/function.h
+++
@@ -1,7 +0,0 @@
-void main_menu(void)
-{
-    printf("Please chose one of the next options:\n");
-    printf("1) Copiring files\t\t\t 2) Moving files\n"
-           "3) Removing files\t\t\t 4) Exit from programm\n");
-    printf("Enter the number of chose menu: ");
-}
