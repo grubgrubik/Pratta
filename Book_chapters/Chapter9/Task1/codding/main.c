78a6502ecff670db9180f1fb94daa9f0989f1afa
grubgrubik <grubastuk@gmail.com>
2021-10-16 15:47:09 +0300

    complete task1

--- Book_chapters/Chapter9/Task1/codding/main.c
+++ Book_chapters/Chapter9/Task1/codding/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 
-int main()
+double min (double x, double y);
+
+int main(void)
 {
-    printf("Hello world!\n");
+
+    double a;
+    double b;
+
+    printf("enter two values for compare:\n");
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+
+    printf("the lesser value is: %.2lf\n", min (a, b));
+
+
     return 0;
+}
+
+
+double min (double x, double y)
+{
+    double tmp = 0.;
+    (x < y) ? (tmp = x) : (tmp = y);
+
+    return tmp;
 }
