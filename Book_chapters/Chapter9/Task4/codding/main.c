91d37a06d7f8c4288411217bd7f045d25344ac04
grubgrubik <grubastuk@gmail.com>
2021-10-17 08:10:43 +0300

    finish task4

--- Book_chapters/Chapter9/Task4/codding/main.c
+++ Book_chapters/Chapter9/Task4/codding/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+double av_harm_val (double a, double b);    //average harmonic value
+
+int main(void)
 {
-    printf("Hello world!\n");
+    double a = 6.;
+    double b = 9.;
+    double ans;
+
+    ans = av_harm_val(a, b);
+
+    printf("the average harmonic value of numbers %.2lf and %.2lf is %.2lf", a, b, ans);
+
+
     return 0;
+}
+
+
+double av_harm_val (double a, double b)
+{
+    a = 1 / a;
+    b = 1 / b;
+
+    return (1 / ((a + b) / 2));
 }
