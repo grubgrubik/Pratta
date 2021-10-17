f95d82bb11fda4786910246d681b7d6fd33a36fc
grubgrubik <grubastuk@gmail.com>
2021-10-17 08:35:46 +0300

    finash task5

--- Book_chapters/Chapter9/Task5/codding/main.c
+++ Book_chapters/Chapter9/Task5/codding/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+void p_repl_max_val(double *a, double *b);
+
+int main(void)
 {
-    printf("Hello world!\n");
+    double a, b, tmp;
+
+    printf("enter two floating point numbers:\n");
+
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+
+    printf("your entered values: a = %.3lf, b = %.3lf\n", a, b);
+    printf("\nreplacing lesser value to bigger number...\n");
+
+    p_repl_max_val(&a, &b);
+
+    printf("new values are: a = %.3lf, b = %.3lf\n\n", a, b);
+
     return 0;
+}
+
+
+void p_repl_max_val(double *a, double *b)
+{
+
+    (*a > *b) ? (*b = *a) : (*a = *b);
 }
