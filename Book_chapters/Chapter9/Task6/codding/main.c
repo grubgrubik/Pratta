9ed067ffdf16c9d93c1415234373b52458776995
grubgrubik <grubastuk@gmail.com>
2021-10-17 12:15:49 +0300

    finish task6

--- Book_chapters/Chapter9/Task6/codding/main.c
+++ Book_chapters/Chapter9/Task6/codding/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+//   a-z  --> 97-122
+//   A-Z  --> 65-90
+
+int return_numb(char symb);
+
+int main(void)
 {
-    printf("Hello world!\n");
+    char ch;
+
+    while ((ch = getchar()) != EOF)
+    {
+        if (ch == 26)
+            break;
+        if (ch == '\n')
+            printf("symbol 'Enter' - his number is %d\n", return_numb(ch));
+        else
+            printf("symbol %c - his number is %d\n", ch, return_numb(ch));
+
+
+    }
+
+    printf("i knew that i would do it\n");
+
     return 0;
+}
+
+
+int return_numb(char symb)
+{
+    int number;
+
+    if (symb >= 'a' && symb <= 'z')
+        number = symb - 96;
+    else if (symb >= 'A' && symb <= 'Z')
+        number = symb - 64;
+    else
+        number = 0;
+
+    return number;
+
 }
