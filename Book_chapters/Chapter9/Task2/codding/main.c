f8ab113146258a6379a5c5fd95470def3d1ccd60
grubgrubik <grubastuk@gmail.com>
2021-10-17 06:45:40 +0300

    done task2

--- Book_chapters/Chapter9/Task2/codding/main.c
+++ Book_chapters/Chapter9/Task2/codding/main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void chline (char ch, int i, int j);
+
+int main(void)
 {
-    printf("Hello world!\n");
+    int colum, row, key;
+    char symbol, tmp_ch;
+    key = 1;
+    do
+    {
+        printf("\nenter symbol for drawing: ");
+        symbol = getchar();
+
+        printf("enter quantity of colums and rows, or enter two '0' for exit: ");
+        scanf("%d %d", &colum, &row);
+
+        if ((tmp_ch = getchar()) == '\n')
+               putchar(tmp_ch);
+
+        if (colum == 0 && row == 0)
+        {
+            printf("really exit?, enter '0' to exit or '1' to continue: ");
+            scanf("%d", &key);
+        }
+        else
+            chline(symbol, colum, row);
+
+
+    }   while (key == 1);
+
+
+
     return 0;
+}
+
+
+
+
+void chline (char ch, int i, int j)
+{
+    for (int x = 0; x < j; x++)
+    {
+        for (int y = 0; y < i; y++)
+            printf("%c", ch);
+
+        putchar('\n');
+    }
 }
