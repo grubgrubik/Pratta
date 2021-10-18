0085fe3baf7950161f71deba9045681aa38b3edd
grubgrubik <grubastuk@gmail.com>
2021-10-18 14:40:46 +0300

    finish task7

--- Book_chapters/Chapter9/Task7/codding/main.c
+++ Book_chapters/Chapter9/Task7/codding/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+float pow_func(float n, int exp);
+
+
+int main(void)
 {
-    printf("Hello world!\n");
+
+    float result, digit;
+    int exp;
+
+    printf("Enter number and integer power\n"
+           "to switch the number will be riser");
+    printf("press 'q' to exit:\n");
+
+    while (scanf("%f %d", &digit, &exp) == 2)
+    {
+        result = pow_func(digit, exp);
+
+        printf("%.3f to the power %d is %.3f\n", digit, exp, result);
+
+        printf("Enter the next pair of pairs or 'q' to quit:\n");
+    }
+
+
+    printf("i knew that i would do it\n");
     return 0;
+}
+
+
+
+float pow_func(float n, int exp)
+{
+    float result = 1;
+
+    if (n == 0)
+        result = 0;
+    else if (exp == 0)
+        result = 1;
+    else if (exp < 0)
+    {
+        for (int i = 1; i <= exp * -1; i++)
+            result *= n;
+            result = 1 / result;
+    }
+    else
+        for (int i = 1; i <= exp; i++)
+            result *= n;
+
+    return result;
 }
