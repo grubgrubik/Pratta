b113ffdcd8e26e47df00fd3a3971f65c6acc73c3
grubgrubik <grubastuk@gmail.com>
2021-10-21 21:46:07 +0300

    finish task9

--- Book_chapters/Chapter9/Task9/codding/main.c
+++ Book_chapters/Chapter9/Task9/codding/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void to_base_n(int digit, int numeral);
+
+int main(void)
 {
-    printf("Hello world!\n");
+    int number, numeral;
+
+    printf("enter the number and digit of numeral system, or press q to exit:\n");
+
+    while ((scanf("%d %d", &number, &numeral)) == 2)
+    {
+        if (numeral < 2 || numeral > 10)
+        {
+            printf("you entered incorrect value of numeral, enter between 2 and 10\n");
+            printf("enter again number and digit of numeral system:\n");
+            continue;
+        }
+        printf("\nthe number %d in %d system = ", number, numeral);
+
+        to_base_n(number, numeral);
+        printf("\n\nenter the next value of number and numeral system, or press q to exit:\n");
+    }
+
+
     return 0;
+}
+
+
+void to_base_n(int digit, int numeral)
+{
+    int result;
+    result = digit % numeral;
+
+    if (digit >= numeral)
+        to_base_n(digit / numeral, numeral);
+
+    printf("%d", result);
+
 }
