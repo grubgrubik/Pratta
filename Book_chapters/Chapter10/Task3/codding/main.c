ede656ec2b288a2de5b460b9401595dd2ab0cd9b
grubgrubik <grubastuk@gmail.com>
2021-11-16 18:21:45 +0200

    finish do task3 in chapter_10

--- Book_chapters/Chapter10/Task3/codding/main.c
+++ Book_chapters/Chapter10/Task3/codding/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int max_value(int *arr, int n);
 
 int main()
 {
-    printf("Hello world!\n");
+    int mass[10] = {2, 6, 89, 654, -3, 435, 65, 801, 12, 72};
+
+    for (int i =0; i < 10; i++)
+    {
+        printf("%d\n", *(mass + i));
+    }
+
+    printf("\nmax value form this massive is: %d\n", max_value(mass, 10));
+
     return 0;
+}
+
+
+
+int max_value(int *arr, int n)
+{
+    int tmp = *arr;
+    for (int i = 0; i < n; i++)
+    {
+        if (*(arr + i) > tmp)
+        {
+            tmp = *(arr + i);
+        }
+    }
+    return tmp;
 }
