f98186a56d2a420d0b821f017907bc46e6344390
grubgrubik <grubastuk@gmail.com>
2021-11-18 17:23:17 +0200

    fix task4

--- Book_chapters/Chapter10/Task4/codding/main.c
+++ Book_chapters/Chapter10/Task4/codding/main.c
@@ -6,7 +6,7 @@ int return_index(double *arr, int n);
 int main(void)
 {
     int ret_index = 2;
-    double mass[10] = {132.1, 54.8, 91.9, 11.1, 43.5, 87.5, 69.3, 87.2, 78.1, 101,7};
+    double mass[10] = {132.1, 54.8, 91.9, 11.1, 43.5, 87.5, 69.3, 87.2, 78.1, 101.7};
 
     ret_index = return_index(mass, 10);
     printf("the max value of that massive is = %.1lf\n"
