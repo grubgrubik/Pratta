12ec96115b18ee0e8c1cec1f1246cafcf35b1021
grubgrubik <grubastuk@gmail.com>
2021-11-19 15:28:18 +0200

    finish task6

--- Book_chapters/Chapter10/Task6/codding/main.c
+++ Book_chapters/Chapter10/Task6/codding/main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+#define C_MASS 11
+
+void show_mass(double arr[], int c);
+void reverse_mass(double arr_old[], double arr_new[], int c);
+
+int main(void)
 {
-    printf("Hello world!\n");
+    // 11 elements of massive
+    double mass_old[C_MASS] = {132.1, 54.8, 91.9, 11.1, 43.5, 87.5, 69.3, 87.2, 78.1, 101.7, 84.2};
+
+    // 10 elements of massive
+    // double mass_old[C_MASS] = {132.1, 54.8, 91.9, 11.1, 43.5, 87.5, 69.3, 87.2, 78.1, 101.7};
+
+    double mass_new[C_MASS];
+
+    printf("Origin massive:\n");
+
+    show_mass(mass_old, C_MASS);
+
+    printf("Reverse massive:\n");
+
+    reverse_mass(mass_old, mass_new, C_MASS);
+    show_mass(mass_new, C_MASS);
+
     return 0;
+}
+
+void show_mass(double arr[], int c)
+{
+    for (int i = 0; i < c; i++)
+    {
+        printf("%.1lf ", arr[i]);
+    }
+    putchar('\n');
+}
+
+void reverse_mass(double arr_old[], double arr_new[], int c)
+{
+    for (int i = 0, j = c; i < c; i++, j--)
+    {
+        arr_new[j - 1] = arr_old[i];
+    }
 }
