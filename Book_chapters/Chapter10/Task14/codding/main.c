3ca23b8fdfd299b2df5343c705f6c0417e861eb6
grubgrubik <grubastuk@gmail.com>
2021-11-23 20:35:17 +0200

    finish task13 and task14

--- Book_chapters/Chapter10/Task14/codding/main.c
+++ Book_chapters/Chapter10/Task14/codding/main.c
@@ -1,66 +1,82 @@
 #include <stdio.h>
 
-#define ROWS 3
-#define COLUMS 5
 
-void show_mass(double arr[][COLUMS], int rows);
-void fill_mass(double arr[][COLUMS], int rows);
 int show_menu(void);
-void line_average_mass(double arr[], int colum);
-double all_average_mass(double arr[][COLUMS], int rows);
-double max_element_mass(double arr[][COLUMS], int rows);
+
+void line_average_mass(int colum, double arr[colum]);
+void fill_mass(int rows, int colum, double arr[rows][colum]);
+
+double max_element_mass(int rows, int colum, double arr[rows][colum]);
+double all_average_mass(int rows, int colum, double arr[rows][colum]);
+void show_mass(int rows, int colum, double arr[rows][colum]);
+
+
 
 int main(void)
 {
+    int colum, rows;
     int menu_choose;
-    int key;
+    int key = 1;
     int empty_mass = 1;
-
     double all_average = 0.;
     double max_value = 0.;
 
-    double mass[ROWS][COLUMS];
+
 
     printf("operations with massive, to start press '1', to exit press '0'\n");
-    scanf("%d", &key);
+
+
+
+    printf("Please set the size for massive:\n");
+    printf("Lines - ");
+    scanf("%d", &rows);
+    printf("Colums - ");
+    scanf("%d", &colum);
+
+    double mass[rows][colum];
+
+
 
     while (key)
     {
+
         if (empty_mass)
         {
-            printf("please fill the massive, it has size %dx%d elements:\n", ROWS, COLUMS);
-            fill_mass(mass, ROWS);
+            printf("please fill the massive, it has size %dx%d elements:\n", rows, colum);
+            fill_mass(rows, colum, mass);
             empty_mass = 0;
         }
+
 
         menu_choose = show_menu();
         printf("\n");
 
+
         switch (menu_choose)
         {
         case 1:
-            for (int i = 0; i < ROWS; i++)
+            for (int i = 0; i < rows; i++)
             {
                 printf("The average value in %d line of massive - ", i);
-                line_average_mass(mass + i, COLUMS);
+                line_average_mass(colum, mass + i);
             }
             printf("\n");
             break;
 
         case 2:
-            all_average = all_average_mass(mass, ROWS);
+            all_average = all_average_mass(rows, colum, mass);
             printf("The average value in massive - %.2f\n", all_average);
             break;
         case 3:
-            max_value = max_element_mass(mass, ROWS);
+            max_value = max_element_mass(rows, colum, mass);
             printf("The biggest element in massive - %.2lf\n", max_value);
             break;
         case 4:
-            printf("please fill the massive, it has size %dx%d elements:\n", ROWS, COLUMS);
-            fill_mass(mass, ROWS);
+            printf("please fill the massive, it has size %dx%d elements:\n", rows, colum);
+            fill_mass(rows, colum, mass);
             break;
         case 5:
-            show_mass(mass, ROWS);
+            show_mass(rows, colum, mass);
             break;
         case 6:
             key = 0;
@@ -92,23 +108,23 @@ int show_menu(void)
 }
 
 
-void fill_mass(double arr[][COLUMS], int rows)
+void fill_mass(int rows, int colum, double arr[rows][colum])
 {
     for (int i = 0; i < rows; i++)
     {
-        printf("press %d elements in %d row:\n", COLUMS, i);
-        for (int j = 0; j < COLUMS; j++)
+        printf("press %d elements in %d row:\n", colum, i);
+        for (int j = 0; j < colum; j++)
         {
             scanf("%lf", &arr[i][j]);
         }
     }
 }
 
-void show_mass(double arr[][COLUMS], int rows)
+void show_mass(int rows, int colum, double arr[rows][colum])
 {
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < COLUMS; j++)
+        for (int j = 0; j < colum; j++)
         {
             printf("%.2lf ", arr[i][j]);
         }
@@ -118,7 +134,7 @@ void show_mass(double arr[][COLUMS], int rows)
 }
 
 
-void line_average_mass(double arr[], int colum)
+void line_average_mass(int colum, double arr[colum])
 {
     double res = 0.;
     for (int i = 0; i < colum; i++)
@@ -128,27 +144,27 @@ void line_average_mass(double arr[], int colum)
     printf("%.2lf\n", res / colum);
 }
 
-double all_average_mass(double arr[][COLUMS], int rows)
+double all_average_mass(int rows, int colum, double arr[rows][colum])
 {
     double res = 0.;
 
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < COLUMS; j++)
+        for (int j = 0; j < colum; j++)
         {
             res += arr[i][j];
         }
     }
-    return res / (rows * COLUMS);
+    return res / (rows * colum);
 }
 
-double max_element_mass(double arr[][COLUMS], int rows)
+double max_element_mass(int rows, int colum, double arr[rows][colum])
 {
     double max_val = arr[0][0];
 
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < COLUMS; j++)
+        for (int j = 0; j < colum; j++)
         {
             if (max_val < arr[i][j])
             {
