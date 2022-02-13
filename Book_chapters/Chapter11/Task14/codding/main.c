7dbffd5a50e0641e8a2427b06f289998f8cfc6f0
grubgrubik <grubastuk@gmail.com>
2022-02-13 23:11:23 +0200

    finist do task14 in chapter11

--- Book_chapters/Chapter11/Task14/codding/main.c
+++ Book_chapters/Chapter11/Task14/codding/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+	double result = 1.;
+
+	int i;
+
+    if(argc == 3)
+    {
+    	for(i = 0; i < atoi(*(argv + 2)); i++)
+        {
+            result *= atof(*(argv + 1));
+        }
+		puts("-----------------------------------------------------------------------");
+		printf("result: %.2f^%d = %.2f\n", atof(*(argv + 1)), atoi(*(argv + 2)), result);
+		puts("-----------------------------------------------------------------------");
+    }
+    else
+	{
+		printf("incorrect arguments of function!!\n");
+	}
     return 0;
 }
